e54c953fa613b422a0be86fef77e97c7518fff81
acer <wongsatorn1501003@gmail.com>
2019-10-17 23:02:40 +0700

    18

---
+++ app/src/main/java/com/example/flickrbrowser/GetFlickrJsonData.kt
@@ -0,0 +1,40 @@
+package com.example.flickrbrowser
+
+import android.os.AsyncTask
+import android.util.Log
+import org.json.JSONObject
+import java.lang.Exception
+
+class GetFlickrJsonData(private val listener:onDataAvvailable):AsyncTask<String,Void,ArrayList<Photo>>() {
+    private val TAG = "GetFlickrJsonData"
+
+    interface onDataAvvailable{
+        fun onDataAvailable(data: List<Photo>)
+        fun onError(exception: Exception)
+    }
+
+    override fun onPostExecute(result: Array<Photo>?) {
+        Log.d(TAG,"onPostExecute starts")
+        super.onPostExecute(result)
+    }
+
+    override fun doInBackground(vararg params: String?): ArrayList<Photo> {
+        Log.d(TAG,"doInBackground starts")
+        try {
+            val jsonData =JSONObject(params[0])
+            val itemArray = jsonData.getJSONArray("item")
+
+            for (i in 0 until itemArray.length()){
+                val JsonPhoto = itemArray.getJSONObject(i)
+                val title = JsonPhoto.getString("title")
+                val author =JsonPhoto.getString("author")
+                val authorId = JsonPhoto.getString("author_id")
+                val tags = JsonPhoto.getString("tags")
+
+                val jsonMedia = JsonPhoto.getString("media")
+                val photoUrl =JsonPhoto.getString("m")
+                val link = photoUrl.replaceFirst("_m.jpg","_b.jpg")
+            }
+        }
+    }
+}
