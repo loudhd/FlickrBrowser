4a0d319737c9fa7eeed5359bd5ff4cd848d02562
acer <wongsatorn1501003@gmail.com>
2019-10-17 22:33:43 +0700

    15

---
+++ app/src/main/java/com/example/flickrbrowser/Photo.kt
@@ -0,0 +1,7 @@
+package com.example.flickrbrowser
+
+import android.icu.text.CaseMap
+
+class Photo(val title: String,val author:String,val authorId:String,val link:String,val tags:String,val image:String) {
+    
+}
