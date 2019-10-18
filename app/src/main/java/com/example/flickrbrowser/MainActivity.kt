94270df1c5fd066326e1e5112ef70b8aa9b27320
acer <wongsatorn1501003@gmail.com>
2019-10-18 21:58:48 +0700

    21

--- app/src/main/java/com/example/flickrbrowser/MainActivity.kt
+++ app/src/main/java/com/example/flickrbrowser/MainActivity.kt
@@ -10,37 +10,27 @@ import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(),GetRawData.onDownloadComplete {
-    override fun onDownloadComplete(data: String, status: DownloadStatus) {
-        if (status==DownloadStatus.OK){
-            Log.d(TAG,"onDownloadComplete called,data is $data")
-        }else{
-            // download failed
-            Log.d(TAG,"onDownloadCompleted failed with status $status. Error message is:$data")
-        }
-    }
+class MainActivity : AppCompatActivity(), GetRawData.onDownloadComplete {
 
     private val TAG = "MainActivity"
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG,"onCreate calles")
+        Log.d(TAG, "onCreate calles")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
 
-
         val getRawData = GetRawData(this)
         getRawData.execute("https://api.flikr.com/services/feeds/photos_pubilc.gne?tags=android.oreo&format=json&soncallback=1")
-        
-        Log.d(TAG,"onCreate ends")
+
+        Log.d(TAG, "onCreate ends")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        Log.d(TAG,"onCreateOptionsMenu calles")
+        Log.d(TAG, "onCreateOptionsMenu calles")
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
@@ -49,7 +39,7 @@ class MainActivity : AppCompatActivity(),GetRawData.onDownloadComplete {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        Log.d(TAG,"onOptionsItemSalected calles")
+        Log.d(TAG, "onOptionsItemSalected calles")
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
@@ -59,5 +49,16 @@ class MainActivity : AppCompatActivity(),GetRawData.onDownloadComplete {
     /*companion object{
         private const val TAG = "MainActivity"
     }*/
+    override fun onDownloadComplete(data: String, status: DownloadStatus) {
+        if (status == DownloadStatus.OK) {
+            Log.d(TAG, "onDownloadComplete called,data is $data")
+            val getFlickrJsonData = GetFlickrJsonData(this)
+            getFlickrJsonData.execute(data)
+
+        } else {
+            // download failed
+            Log.d(TAG, "onDownloadCompleted failed with status $status. Error message is:$data")
+        }
+    }
 
 }
